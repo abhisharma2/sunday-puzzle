27ab0afef3b5e635838ee900692dbc1f77da8fea
Abhi Sharma <abhisharma@Abhis-MacBook-Pro-2.local>
2022-07-10 07:51:43 -0400

    start work on api

---
+++ api/get-puzzle.js
@@ -0,0 +1,15 @@
+/*
+*  Go to https://www.npr.org/series/4473090/sunday-puzzle
+*  Find latest Puzzle link
+*/
+
+// Find div with id #featured
+// Find first link, grab url, store it
+
+/*
+*  Go to latest Puzzle link
+*  Determine the Puzzle question
+*/
+
+// Find strong tag with "This week's challenge"
+// Regex to grab the text after
